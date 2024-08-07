182319d92d5fb431d0f4145a508d05db1b218f81
Ranaweera-HD <himathdanindu2006@gmail.com>
2024-08-07 13:15:59 +0530

    first commit

---
+++ Online Book Story.java
@@ -0,0 +1,35 @@
+import java.util.*;
+import java.time.LocalDate;
+class OnlineBookStory {
+	public static void main(String[] args){
+		Scanner input = new Scanner(System.in);
+   /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	double CR_Book = 80.00;
+	double Pen = 20.00;
+	double Pencil = 15.00;
+	double Eraser = 10.00;
+	double Glue = 40.00;
+	double Pencil_Box = 100.00;
+	double Water_Bottle = 230.00;
+	double Lunch_Box = 250.00;
+	double Calculator = 250.00;
+	double School_Bag = 1000.00;
+	double Ruler = 10.00;
+	double Highlighter = 50.00;
+	double Cardboard_File = 25.00;
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	System.out.println(" ");
+	System.out.println("\t\t\t\t______             _      _____ _                  ");
+	System.out.println("\t\t\t\t| ___ \\           | |    /  ___| |                 ");
+	System.out.println("\t\t\t\t| |_/ / ___   ___ | | __ \\ `--.| |__   ___  _ __   ");
+	System.out.println("\t\t\t\t| ___ \\/ _ \\ / _ \\| |/ /  `--. \\ '_ \\ / _ \\| '_ \\  ");
+	System.out.println("\t\t\t\t| |_/ / (_) | (_) |   <  /\\__/ / | | | (_) | |_) | ");
+	System.out.println("\t\t\t\t\\____/ \\___/ \\___/|_|\\_\\ \\____/|_| |_|\\___/| .__/  ");
+	System.out.println("\t\t\t\t                                           | |     ");
+	System.out.println("\t\t\t\t                                           |_|     ");
+	System.out.println("\t\t\t\t                                                   ");
+	System.out.println("\t\t\t================================================================");
+	System.out.println("\n");
+	     
+    }
+}
